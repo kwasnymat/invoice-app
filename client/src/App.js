52787aa9c07908c5680457cbdedb6c6840c86367
unknown <krolczyk.artur@gmail.com>
2020-10-20 18:01:55 +0200

    added container

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import { Nav, Form, Button } from 'react-bootstrap';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <div className='elo'></div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
