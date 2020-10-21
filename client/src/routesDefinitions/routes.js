438f73e41b219b75a6c68d0b8f1958652ba983bd
unknown <krolczyk.artur@gmail.com>
2020-10-21 16:38:31 +0200

    added navigation and invoice component

---
+++ client/src/routesDefinitions/routes.js
@@ -0,0 +1,24 @@
+const routes = {
+  home: {
+    name: 'Home',
+    link: '/',
+  },
+  createInvoice: {
+    name: 'Create Invoice',
+    link: '/create-invoice',
+  },
+  yourInvoices: {
+    name: 'Your Invoices',
+    link: '/invoices',
+  },
+  login: {
+    name: 'Login',
+    link: '/login',
+  },
+  signup: {
+    name: 'Sign up',
+    link: '/sign-up',
+  },
+};
+
+export default routes;
