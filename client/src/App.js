61aa0883c28b6e5971a68888a05615b8d708f67b
unknown <krolczyk.artur@gmail.com>
2020-10-21 16:36:44 +0200

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,32 @@ import React from 'react';
 
 import { Nav, Form, Button } from 'react-bootstrap';
 
-import './App.css';
+import Navigation from './components/navigation/Navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Invoices from './components/invoices/Invoices';
+
+import routes from './routesDefinitions/routes';
+
+import './App.scss';
 
 const App = () => {
+  const { home, createInvoice, yourInvoices, login, signup } = routes;
   return (
     <div>
-      <div className='container'>
-        <div className='elo'></div>
-      </div>
+      <Router>
+        <div className='container'>
+          <Navigation />
+          <Switch>
+            <Route path={createInvoice.link} />
+            <Route exact path={home.link} />
+            <Route path={login.link} />
+            <Route path={yourInvoices.link} component={Invoices} />
+            <Route path={signup.link} />
+            <Route />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 };
