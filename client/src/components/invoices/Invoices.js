cd277c1131c5290a8e7ce3a9a28cee0318b0e1ac
unknown <krolczyk.artur@gmail.com>
2020-10-21 16:38:15 +0200

    added navigation and invoice component

---
+++ client/src/components/invoices/Invoices.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Invoices = () => {
+  return <div className='invoces'>elooo</div>;
+};
+export default Invoices;
---
+++ client/src/components/navigation/Navigation.js
@@ -0,0 +1,61 @@
+import React, { useState, useCallback } from 'react';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
+
+import routes from '../../routesDefinitions/routes';
+import logo from '../../assets/logo.png';
+
+import './Navigation.scss';
+
+const Navigation = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpandChange = useCallback(() => {
+    setExpanded(false);
+  }, []);
+
+  const handleNavChange = useCallback(() => {
+    setExpanded((prevExpanded) => !prevExpanded);
+  }, []);
+
+  const { createInvoice, yourInvoices, login, signup, home } = routes;
+
+  const navigationLinks = [
+    { name: createInvoice.name, link: createInvoice.link },
+    { name: yourInvoices.name, link: yourInvoices.link },
+    { name: login.name, link: login.link },
+    { name: signup.name, link: signup.link },
+  ];
+
+  const renderNavLinks = () =>
+    navigationLinks.map(({ name, link }) => (
+      <div className='nav__link nav-link' key={name}>
+        <NavLink className='inactive' activeClassName='active' to={link}>
+          {name}
+        </NavLink>
+      </div>
+    ));
+
+  return (
+    <div className='Navigation'>
+      <Navbar expanded={expanded} fixed='top' bg='light' expand='md'>
+        <NavLink to='/' className='nav__brand' exact to={home.link}>
+          <Navbar.Brand>
+            <img className='navbar-brand' src={logo} alt='invoiceApp logo' />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle
+          onClick={handleNavChange}
+          aria-controls='basic-navbar-nav'
+        />
+        <Navbar.Collapse>
+          <Nav onClick={handleExpandChange} className='ml-auto'>
+            {renderNavLinks()}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Navigation;
