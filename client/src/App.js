c56f685fad168ffc0e2ee5c72415bcf62127e419
unknown <krolczyk.artur@gmail.com>
2020-10-22 21:20:48 +0200

    added create invoice route

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Invoices from './components/invoices/Invoices';
 import Footer from './components/footer/Footer';
+import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
+import Invoice from './components/createInvoice/CreateInvoice';
 
 import routes from './routesDefinitions/routes';
 
@@ -15,22 +18,20 @@ import './App.scss';
 const App = () => {
   const { home, createInvoice, yourInvoices, login, signup } = routes;
   return (
-    <div>
-      <Router>
-        <Navigation />
-        <div className='container content'>
-          <Switch>
-            <Route path={createInvoice.link} />
-            <Route exact path={home.link} />
-            <Route path={login.link} />
-            <Route path={yourInvoices.link} component={Invoices} />
-            <Route path={signup.link} />
-            <Route />
-          </Switch>
-        </div>
-        <Footer />
-      </Router>
-    </div>
+    <Router>
+      <Navigation />
+      <div className='container'>
+        <Switch>
+          <Route exact path={home.link} />
+          <Route path={createInvoice.link} component={Invoice} />
+          <Route path={login.link} component={Login} />
+          <Route path={yourInvoices.link} component={Invoices} />
+          <Route path={signup.link} component={Signup} />
+          <Route />
+        </Switch>
+      </div>
+      <Footer />
+    </Router>
   );
 };
 
