d682861313666bee8b75478cb038ea39dff3299a
unknown <krolczyk.artur@gmail.com>
2020-10-22 21:21:22 +0200

    added footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -3,37 +3,8 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => {
-  const socialLinks = [
-    {
-      name: 'fa fa-facebook-square',
-      href: 'https://www.facebook.com/playsportusa1/',
-    },
-    {
-      name: 'fa fa-instagram',
-      href: 'https://www.instagram.com/playsportusa1/',
-    },
-    {
-      name: 'fa fa-youtube-play',
-      href: 'https://www.youtube.com/channel/UCChDfRK8jPXg0MBuyoBa10g',
-    },
-  ];
-
-  const renderSocialLinks = () =>
-    socialLinks.map(({ name, href }) => (
-      <li className='list-inline-item' key={name}>
-        <a
-          className='list__redirection'
-          href={href}
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <i className={name} />
-        </a>
-      </li>
-    ));
-
   return (
-    <footer className='py-4 bg-dark text-white-50 sticky-footer'>
+    <footer className='py-4 bg-dark text-white-50 sticky-footer fixed-bottom'>
       <div className=' container text-center'>
         <a
           className='footer__info'
