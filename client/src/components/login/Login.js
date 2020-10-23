539fb58ac9b1887dcc561878a50ea491714ca9ba
unknown <krolczyk.artur@gmail.com>
2020-10-23 18:33:42 +0200

    changed nav heigh

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -5,49 +5,49 @@ import './Login.scss';
 
 const Login = () => {
   return (
-    <div class='signup-form'>
+    <div className='signup-form'>
       <form>
         <h2>Member Login</h2>
         <hr />
-        <div class='form-group'>
-          <div class='input-group'>
-            <div class='input-group-prepend'>
-              <span class='input-group-text'>
-                <i class='fa fa-paper-plane'></i>
+        <div className='form-group'>
+          <div className='input-group'>
+            <div className='input-group-prepend'>
+              <span className='input-group-text'>
+                <i className='fa fa-paper-plane'></i>
               </span>
             </div>
             <input
               type='email'
-              class='form-control'
+              className='form-control'
               name='email'
               placeholder='Email Address'
               required='required'
             />
           </div>
         </div>
-        <div class='form-group'>
-          <div class='input-group'>
-            <div class='input-group-prepend'>
-              <span class='input-group-text'>
-                <i class='fa fa-lock'></i>
+        <div className='form-group'>
+          <div className='input-group'>
+            <div className='input-group-prepend'>
+              <span className='input-group-text'>
+                <i className='fa fa-lock'></i>
               </span>
             </div>
             <input
               type='text'
-              class='form-control'
+              className='form-control'
               name='password'
               placeholder='Password'
               required='required'
             />
           </div>
         </div>
-        <div class='form-group'>
-          <button type='submit' class='btn btn-primary btn-lg '>
+        <div className='form-group'>
+          <button type='submit' className='btn btn-primary btn-lg '>
             Sign Up
           </button>
         </div>
       </form>
-      <div class='text-center'>
+      <div className='text-center'>
         <NavLink
           exact
           to='/sign-up'
