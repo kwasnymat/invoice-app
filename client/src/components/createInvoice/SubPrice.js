0e235d2f5451af393219223d5db4cc9fe2cf48b6
unknown <krolczyk.artur@gmail.com>
2020-10-26 23:43:07 +0100

    added child comp to do some math

---
+++ client/src/components/createInvoice/SubPrice.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { useWatch } from 'react-hook-form';
+import { Form } from 'react-bootstrap';
+
+const SubPrice = ({ control, index, register, defaultValue, onChange }) => {
+  const value = useWatch({
+    control,
+    name: `items[${index}]`,
+    defaultValue: {},
+  });
+
+  return (
+    <Form.Control
+      onChange={(e) => onChange(e, index)}
+      defaultValue={defaultValue}
+      ref={register()}
+      name={`items[${index}].totalMoney`}
+      value={(
+        (value.qty || 0) * (value.priceNoVat || 0) +
+        (((value.qty || 0) * (value.priceNoVat || 0)) / 100) * (value.vat || 0)
+      ).toFixed(2)}
+    />
+  );
+};
+
+export default SubPrice;
