6661000ccdd3aafcf447c39f5aed943d82f3966c
unknown <krolczyk.artur@gmail.com>
2020-10-27 00:33:10 +0100

    added child comp to using react hook form calc prices without render

---
+++ client/src/components/createInvoice/NoVatPrice.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { useWatch } from 'react-hook-form';
+import { Form } from 'react-bootstrap';
+
+const NoVatPrice = ({ control, index, register, defaultValue }) => {
+  const value = useWatch({
+    control,
+    name: `items[${index}]`,
+    defaultValue: {},
+  });
+
+  return (
+    <Form.Control
+      readOnly
+      defaultValue={defaultValue}
+      ref={register()}
+      name={`items[${index}].[priceNoVat]`}
+      value={((value.qty || 0) * (value.unitCost || 0)).toFixed(2)}
+    />
+  );
+};
+
+export default NoVatPrice;
