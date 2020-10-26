6ef8f2352c9673359f253ca395e000d4ba35b783
unknown <krolczyk.artur@gmail.com>
2020-10-27 00:33:26 +0100

    added child comp to using react hook form calc prices without render

---
+++ client/src/components/createInvoice/VatPrice.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { useWatch } from 'react-hook-form';
+import { Form } from 'react-bootstrap';
+
+const VatPrice = ({ control, index, register, defaultValue }) => {
+  const value = useWatch({
+    control,
+    name: `items[${index}]`,
+    defaultValue: {},
+  });
+
+  return (
+    <Form.Control
+      readOnly
+      defaultValue={defaultValue}
+      ref={register()}
+      name={`items[${index}].totalMoney`}
+      value={(
+        (value.qty || 0) * (value.unitCost || 0) +
+        (((value.qty || 0) * (value.unitCost || 0)) / 100) * (value.vat || 0)
+      ).toFixed(2)}
+    />
+  );
+};
+
+export default VatPrice;
