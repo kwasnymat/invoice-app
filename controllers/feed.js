035c62ee03844e709bb2c667f0db0c7ed61632a1
unknown <krolczyk.artur@gmail.com>
2020-10-28 18:40:05 +0100

    added func in backend to get data

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,39 @@
 exports.getInvoice = (req, res, next) => {
   res.status(200).json({
-    invoices: [{ invoiceNumber: 'First invoice', content: 'Paid bro' }],
+    invoices: [
+      {
+        _id: '1',
+        invoiceNumber: '238/16',
+        dateInvoice: '2020-01-02',
+        cityInvoice: 'Bielsko-Biała',
+        SellerCompanyName: 'Bax Spółka Akcyjna',
+        SellerCompanyStreet: 'Korfantego 1/12',
+        SellerCompanyZip: '40-004',
+        SellerCompanyCity: 'Katowice',
+        SellerCompanyVat: '928272628262',
+        SellerCompanyPhone: '262526272',
+        BuyerCompanyName: 'Wall District',
+        BuyerCompanyStreet: 'Cansas 20',
+        BuyerCompanyZip: '098272',
+        BuyerCompanyCity: 'Warszawa',
+        BuyerCompanyVat: '82728276282',
+        BuyerCompanyPhone: '827625282',
+        currency: '$',
+        sub_total: '20222',
+        tax_amountTotal: '1200',
+        total_amount: '30200',
+        items: [
+          {
+            productName: 'Biurko',
+            unitCost: '200',
+            qty: '3',
+            priceNoVat: '100',
+            vat: '23',
+            totalMoney: '200',
+          },
+        ],
+      },
+    ],
   });
 };
 
