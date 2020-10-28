94a4550770fcf3d2caa5fbf344c47dc5f58439f4
unknown <krolczyk.artur@gmail.com>
2020-10-28 18:32:48 +0100

    added fake invoice to tryy get

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Acces-Control-Allow-Origin', '*');
-  res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Acces-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
 
