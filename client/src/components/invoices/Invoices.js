12ba6a304dca7675c4ac9c0a97ad8718a34d2d18
unknown <krolczyk.artur@gmail.com>
2020-10-29 21:34:09 +0100

    added navlink with invoiceId

--- client/src/components/invoices/Invoices.js
+++ client/src/components/invoices/Invoices.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { fetchInvoices } from '../store/actions';
 
-import { fetchInvoices } from './store/actions';
+import { Table } from 'react-bootstrap';
 
-import { Card, CardDeck } from 'react-bootstrap';
+import './Invoices.scss';
 
 const Invoices = () => {
   const { invoices } = useSelector(({ invoices }) => invoices);
@@ -16,38 +18,59 @@ const Invoices = () => {
   console.log(invoices);
 
   const generaterInvoices = () =>
-    invoices.map((invoice) => (
-      <tbody key={invoice._id}>
-        <tr id='addr0'>
-          <td>a</td>
-          <td>a</td>
-          <td>a</td>
-          <td>a</td>
-          <td>a</td>
-          <td>a</td>
-          <td>a </td>
-          <td>a</td>
-        </tr>
-      </tbody>
-    ));
+    invoices.map((invoice, id) => {
+      const {
+        invoiceNumber,
+        dateInvoice,
+        BuyerCompanyName,
+        tax_amountTotal,
+        total_amount,
+        currency,
+        _id,
+      } = invoice;
+      return (
+        <tbody key={_id}>
+          <tr>
+            <td className='text-center'>{id + 1}</td>
+            <td className='text-center'>{invoiceNumber}</td>
+            <td className='text-center'>{dateInvoice}</td>
+            <td className='text-center'>{BuyerCompanyName} </td>
+            <td className='text-center'>
+              {tax_amountTotal} {currency}
+            </td>
+            <td className='text-center'>
+              {total_amount} {currency}
+            </td>
+            <td className='text-center'>
+              <NavLink to={`/invoices/${_id}`}>
+                <span className='glyphicon glyphicon-edit'></span> Edit
+              </NavLink>
+              <a href='/' className='btn btn-danger btn-xs'>
+                <span className='glyphicon glyphicon-remove'></span> Del
+              </a>
+            </td>
+          </tr>
+        </tbody>
+      );
+    });
 
   return (
     <div className='row'>
       <div className='col-lg-12'>
-        <table className='table table-bordered table-hover' id='tab_logic'>
+        <Table responsive='sm custab'>
           <thead>
             <tr>
+              <th className='text-center'>#</th>
               <th className='text-center'> Invoice number</th>
-              <th className='text-center'> Product name</th>
-              <th className='text-center'> Unit Cost</th>
-              <th className='text-center'> Qty </th>
-              <th className='text-center'> Price excluding VAT</th>
-              <th className='text-center'> VAT %</th>
-              <th className='text-center'> Price including VAT </th>
+              <th className='text-center'> Invoice date</th>
+              <th className='text-center'> Recipient</th>
+              <th className='text-center'> Vat amount </th>
+              <th className='text-center'> Total amount</th>
+              <th className='text-center'> Action</th>
             </tr>
           </thead>
           {generaterInvoices()}
-        </table>
+        </Table>
       </div>
     </div>
   );
