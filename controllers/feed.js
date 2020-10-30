d0acfbcb432e8f865bb7233edb5238c5f2bbb26e
unknown <krolczyk.artur@gmail.com>
2020-10-30 22:38:27 +0100

    added controler in bc for edit inv

--- controllers/feed.js
+++ controllers/feed.js
@@ -113,3 +113,83 @@ exports.getInvoice = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.updateInvoice = async (req, res, next) => {
+  const invoiceId = req.params.invoiceId;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed, entered data is incorrect.');
+    error.statusCode = 422;
+    throw error;
+  }
+  const {
+    invoiceNumber,
+    dateInvoice,
+    cityInvoice,
+    SellerCompanyName,
+    SellerCompanyStreet,
+    SellerCompanyZip,
+    SellerCompanyCity,
+    SellerCompanyVat,
+    SellerCompanyPhone,
+    BuyerCompanyName,
+    BuyerCompanyStreet,
+    BuyerCompanyZip,
+    BuyerCompanyCity,
+    BuyerCompanyVat,
+    BuyerCompanyPhone,
+    currency,
+    sub_total,
+    tax_amountTotal,
+    total_amount,
+    items,
+    productName,
+    unitCost,
+    qty,
+    priceNoVat,
+    vat,
+    totalMoney,
+  } = req.body;
+  try {
+    const invoice = await Invoice.findById(invoiceId);
+    if (!invoice) {
+      const error = new Error('Could not find invoice.');
+      error.statusCode = 404;
+      throw error;
+    }
+    invoice.invoiceNumber = invoiceNumber;
+    invoice.dateInvoice = dateInvoice;
+    invoice.cityInvoice = cityInvoice;
+    invoice.SellerCompanyName = SellerCompanyName;
+    invoice.SellerCompanyStreet = SellerCompanyStreet;
+    invoice.SellerCompanyZip = SellerCompanyZip;
+    invoice.SellerCompanyCity = SellerCompanyCity;
+    invoice.SellerCompanyVat = SellerCompanyVat;
+    invoice.SellerCompanyPhone = SellerCompanyPhone;
+    invoice.BuyerCompanyName = BuyerCompanyName;
+    invoice.BuyerCompanyStreet = BuyerCompanyStreet;
+    invoice.BuyerCompanyZip = BuyerCompanyZip;
+    invoice.BuyerCompanyCity = BuyerCompanyCity;
+    invoice.BuyerCompanyVat = BuyerCompanyVat;
+    invoice.BuyerCompanyPhone = BuyerCompanyPhone;
+    invoice.currency = currency;
+    invoice.sub_total = sub_total;
+    invoice.tax_amountTotal = tax_amountTotal;
+    invoice.total_amount = total_amount;
+    invoice.items = items;
+    invoice.productName = productName;
+    invoice.unitCost = unitCost;
+    invoice.qty = qty;
+    invoice.priceNoVat = priceNoVat;
+    invoice.vat = vat;
+    invoice.totalMoney = totalMoney;
+    const result = await invoice.save();
+    res.status(200).json({ message: 'Invoice updated!', invoice: result });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
