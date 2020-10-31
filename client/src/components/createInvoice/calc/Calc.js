1c0075668934bb46556e1f13af521031fa07fadb
unknown <krolczyk.artur@gmail.com>
2020-10-31 19:35:37 +0100

    added functions to handle total score

---
+++ client/src/components/createInvoice/calc/Calc.js
@@ -0,0 +1,28 @@
+let sum = (a) => a.reduce((x, y) => x + y);
+
+export const CalcGrantTotal = ({ setValue, watch }) => {
+  const items = watch('items');
+  console.log(items);
+  let sumAmount = sum(items.map((x) => Number(x.totalMoney)));
+  const outputGrantTotal = sumAmount.toFixed(2);
+  setValue('total_amount', outputGrantTotal);
+  return null;
+};
+
+export const CalcSubTotal = ({ setValue, watch }) => {
+  const items = watch('items');
+  let sumAmount = sum(items.map((x) => Number(x.priceNoVat)));
+  const outputSubTotal = sumAmount.toFixed(2);
+  setValue('sub_total', outputSubTotal);
+
+  return null;
+};
+
+export const CalcTaxTotal = ({ setValue, watch }) => {
+  const items = watch('items');
+  let sumGrandTotal = sum(items.map((x) => Number(x.totalMoney)));
+  let sumSubTotal = sum(items.map((x) => Number(x.priceNoVat)));
+  const outputTaxTotal = (sumGrandTotal - sumSubTotal).toFixed(2);
+  setValue('tax_amountTotal', outputTaxTotal);
+  return null;
+};
