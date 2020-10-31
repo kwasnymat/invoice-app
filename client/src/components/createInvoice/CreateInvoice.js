71ee0e0d6f20caca5ed338c05a518a28a25336f7
unknown <krolczyk.artur@gmail.com>
2020-10-31 19:33:31 +0100

    changed way to do math with total value. proper way

--- client/src/components/createInvoice/CreateInvoice.js
+++ client/src/components/createInvoice/CreateInvoice.js
@@ -8,11 +8,31 @@ import CreateInvoiceForm from './CreateInvoiceForm';
 import { addInvoice } from '../store/actions';
 
 const Invoices = () => {
-  const { register, control, handleSubmit, watch, errors } = useForm();
+  const {
+    register,
+    control,
+    handleSubmit,
+    watch,
+    errors,
+    setValue,
+  } = useForm();
   const { fields, append, remove } = useFieldArray({
     control,
     name: 'items',
   });
+
+  const defaultValues = {
+    test: [
+      {
+        value: '0',
+        nestedArray: [{ value: '0' }],
+      },
+      {
+        value: '0',
+        nestedArray: [{ value: '1' }],
+      },
+    ],
+  };
 
   const dispatch = useDispatch();
 
@@ -25,29 +45,42 @@ const Invoices = () => {
     dispatch(addInvoice(invoice));
   };
   const items = watch('items');
-  console.log(items);
 
-  const calcGrantTotal = () => {
-    let sum = (a) => a.reduce((x, y) => x + y);
-    let sumAmount = sum(items.map((x) => Number(x.totalMoney)));
-    return sumAmount.toFixed(2);
-  };
+  //   const calcGrantTotal = () => {
+  //     let sum = (a) => a.reduce((x, y) => x + y);
+  //     let sumAmount = sum(items.map((x) => Number(x.totalMoney)));
+  //     const outputGrantTotal = sumAmount.toFixed(2);
+  //     setValue('total_amount', outputGrantTotal);
+  //     // console.log(outputGrantTotal);
 
-  const calcSubTotal = () => {
-    let sum = (a) => a.reduce((x, y) => x + y);
-    let sumAmount = sum(items.map((x) => Number(x.priceNoVat)));
-    return sumAmount.toFixed(2);
-  };
+  //     return outputGrantTotal;
+  //   };
 
-  const calcTaxTotal = () => {
-    let sum = (a) => a.reduce((x, y) => x + y);
-    let sumGrandTotal = sum(items.map((x) => Number(x.totalMoney)));
-    let sumSubTotal = sum(items.map((x) => Number(x.priceNoVat)));
-    return (sumGrandTotal - sumSubTotal).toFixed(2);
-  };
+  //   const calcSubTotal = () => {
+  //     let sum = (a) => a.reduce((x, y) => x + y);
+  //     let sumAmount = sum(items.map((x) => Number(x.priceNoVat)));
+  //     const outputSubTotal = sumAmount.toFixed(2);
+  //     setValue('sub_total', outputSubTotal);
+  //     // console.log(outputSubTotal);
+
+  //     return outputSubTotal;
+  //   };
+
+  //   const calcTaxTotal = () => {
+  //     let sum = (a) => a.reduce((x, y) => x + y);
+  //     let sumGrandTotal = sum(items.map((x) => Number(x.totalMoney)));
+  //     let sumSubTotal = sum(items.map((x) => Number(x.priceNoVat)));
+  //     const outputTaxTotal = (sumGrandTotal - sumSubTotal).toFixed(2);
+  //     setValue('tax_amountTotal', outputTaxTotal);
+  //     // console.log(outputTaxTotal);
+  //     return null;
+  //   };
 
   return (
     <CreateInvoiceForm
+      defaultValues={defaultValues}
+      watch={watch}
+      setValue={setValue}
       errors={errors}
       control={control}
       handleSubmit={handleSubmit}
@@ -56,9 +89,6 @@ const Invoices = () => {
       fields={fields}
       onSubmit={onSubmit}
       currency={currency}
-      calcGrantTotal={calcGrantTotal}
-      calcSubTotal={calcSubTotal}
-      calcTaxTotal={calcTaxTotal}
       register={register}
       setCurrency={setCurrency}
       currencies={currencies}
