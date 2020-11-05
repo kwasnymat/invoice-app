425efd71dc964da1fdb33b17d1aff7f3d1a12e1d
unknown <krolczyk.artur@gmail.com>
2020-11-05 17:26:13 +0100

    deleted navbar

--- client/src/components/auth/signup/Signup.js
+++ client/src/components/auth/signup/Signup.js
@@ -6,65 +6,65 @@ import { NavLink } from 'react-router-dom';
 
 const Signup = () => {
   return (
-    <div class='signup-form'>
+    <div className='signup-form'>
       <form>
         <h2>Sign Up</h2>
         <hr />
-        <div class='form-group'>
-          <div class='input-group'>
-            <div class='input-group-prepend'>
-              <span class='input-group-text'>
-                <span class='fa fa-user' />
+        <div className='form-group'>
+          <div className='input-group'>
+            <div className='input-group-prepend'>
+              <span className='input-group-text'>
+                <span className='fa fa-user' />
               </span>
             </div>
             <input
               type='text'
-              class='form-control'
+              className='form-control'
               name='username'
               placeholder='Username'
               required='required'
             />
           </div>
         </div>
-        <div class='form-group'>
-          <div class='input-group'>
-            <div class='input-group-prepend'>
-              <span class='input-group-text'>
-                <i class='fa fa-paper-plane'></i>
+        <div className='form-group'>
+          <div className='input-group'>
+            <div className='input-group-prepend'>
+              <span className='input-group-text'>
+                <i className='fa fa-paper-plane'></i>
               </span>
             </div>
             <input
               type='email'
-              class='form-control'
+              className='form-control'
               name='email'
               placeholder='Email Address'
               required='required'
             />
           </div>
         </div>
-        <div class='form-group'>
-          <div class='input-group'>
-            <div class='input-group-prepend'>
-              <span class='input-group-text'>
-                <i class='fa fa-lock'></i>
+        <div className='form-group'>
+          <div className='input-group'>
+            <div className='input-group-prepend'>
+              <span className='input-group-text'>
+                <i className='fa fa-lock'></i>
               </span>
             </div>
             <input
               type='text'
-              class='form-control'
+              className='form-control'
               name='password'
               placeholder='Password'
               required='required'
             />
           </div>
         </div>
-        <div class='form-group'>
-          <button type='submit' class='btn btn-primary btn-lg'>
+        <div className='form-group'>
+          <button type='submit' className='btn btn-primary btn-lg'>
             Sign Up
           </button>
         </div>
       </form>
-      <div class='text-center'>
+      <div className='text-center'>
         Already have an account?
         <NavLink exact to='/login'>
           Login here
