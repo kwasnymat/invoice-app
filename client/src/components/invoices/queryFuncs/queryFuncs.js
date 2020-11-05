7ca3ffd409a485c983a6855cfab756db6717cec8
unknown <krolczyk.artur@gmail.com>
2020-11-05 17:38:09 +0100

    deleted unnecessary lines

--- client/src/components/invoices/queryFuncs/queryFuncs.js
+++ client/src/components/invoices/queryFuncs/queryFuncs.js
@@ -1,22 +1,15 @@
-export const reduceQueryString = (routeQueryString) => {
-  const url = new URLSearchParams(routeQueryString).toString();
-  return url && `?${url}`;
-};
 export const createQueryString = (values) => {
   const filteredValues = Object.entries(values).filter(
     ([, value]) => value !== '-------'
   );
+  const url = new URLSearchParams(filteredValues).toString();
+  return url && `?${url}`;
+};
 
-  return reduceQueryString(filteredValues);
-};
-export const reduceQueryStore = (routeQueryString) => {
-  const url = new URLSearchParams(routeQueryString).toString();
-  return url && `${url}`;
-};
 export const createQueryStore = (values) => {
   const filteredValues = Object.entries(values).filter(
     ([, value]) => value !== '-------'
   );
-
-  return reduceQueryStore(filteredValues);
+  const url = new URLSearchParams(filteredValues).toString();
+  return url && `${url}`;
 };
