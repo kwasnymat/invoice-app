52fd03e840e716a776845ec12964de2ce31d98a8
unknown <krolczyk.artur@gmail.com>
2020-11-05 17:28:58 +0100

    added type for saving query

--- client/src/components/invoices/store/types.js
+++ client/src/components/invoices/store/types.js
@@ -2,3 +2,4 @@ export const FETCH_INVOICES = 'FETCH_INVOICES';
 export const FETCH_INVOICE = 'FETCH_INVOICE';
 export const DELETE_INVOICE = 'DELETE_INVOICE';
 export const UPDATE_INVOICE = 'UPDATE_INVOICE';
+export const SAVE_QUERY = 'SAVE_QUERY';
