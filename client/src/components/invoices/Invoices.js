349f63dc8f4b63ea6bdaa78167e3e8b244e2d8ff
unknown <krolczyk.artur@gmail.com>
2020-11-05 17:59:50 +0100

    improve handling queries in redux

--- client/src/components/invoices/Invoices.js
+++ client/src/components/invoices/Invoices.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
-import { fetchInvoices, deleteInvoice } from './store/actions';
+import { fetchInvoices, deleteInvoice, saveQuery } from './store/actions';
 import { Table } from 'react-bootstrap';
 import Pagination from 'react-js-pagination';
 import FilterBar from '../layout/filterBar/FilterBar';
@@ -21,6 +21,7 @@ const Invoices = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchInvoices());
+    dispatch(saveQuery(''));
   }, [dispatch]);
 
   const deleteInvoiceHandler = (invoiceId) => {
@@ -33,45 +34,57 @@ const Invoices = () => {
   };
 
   const totalItems = totalPages * invoices.length;
-  const generaterInvoices = () =>
-    invoices.map((invoice) => {
-      const {
-        invoiceNumber,
-        dateInvoice,
-        BuyerCompanyName,
-        tax_amountTotal,
-        total_amount,
-        currency,
-        _id,
-      } = invoice;
+  const generaterInvoices = () => (
+    <Table responsive='sm custab'>
+      <thead className='invoices__thead'>
+        <tr>
+          <th className='text-center'> Invoice number</th>
+          <th className='text-center'> Invoice date</th>
+          <th className='text-center'> Recipient</th>
+          <th className='text-center'> Vat amount </th>
+          <th className='text-center'> Total amount</th>
+          <th className='text-center'> Action</th>
+        </tr>
+      </thead>
+      {invoices.map((invoice) => {
+        const {
+          invoiceNumber,
+          dateInvoice,
+          BuyerCompanyName,
+          tax_amountTotal,
+          total_amount,
+          currency,
+          _id,
+        } = invoice;
 
-      return (
-        <tbody key={_id} className='recipment__details'>
-          <tr>
-            <td className='text-center'>{invoiceNumber}</td>
-            <td className='text-center'>{dateInvoice}</td>
-            <td className='text-center'>{BuyerCompanyName} </td>
-            <td className='text-center'>
-              {tax_amountTotal} {currency}
-            </td>
-            <td className='text-center'>
-              {total_amount} {currency}
-            </td>
-            <td className='text-center'>
-              <NavLink to={`/invoices/${_id}`}>
-                <i className='fas fa-info-circle detailsId_button' />
-              </NavLink>
+        return (
+          <tbody key={_id} className='recipment__details'>
+            <tr>
+              <td className='text-center'>{invoiceNumber}</td>
+              <td className='text-center'>{dateInvoice}</td>
+              <td className='text-center'>{BuyerCompanyName} </td>
+              <td className='text-center'>
+                {tax_amountTotal} {currency}
+              </td>
+              <td className='text-center'>
+                {total_amount} {currency}
+              </td>
+              <td className='text-center'>
+                <NavLink to={`/invoices/${_id}`}>
+                  <i className='fas fa-info-circle detailsId_button' />
+                </NavLink>
 
-              <i
-                onClick={() => deleteInvoiceHandler(_id)}
-                className='fas fa-trash-alt deleteId_button'
-              />
-            </td>
-          </tr>
-        </tbody>
-      );
-    });
-
+                <i
+                  onClick={() => deleteInvoiceHandler(_id)}
+                  className='fas fa-trash-alt deleteId_button'
+                />
+              </td>
+            </tr>
+          </tbody>
+        );
+      })}
+    </Table>
+  );
   const isInvoicesListEmpty = !invoices.length && !isLoading && (
     <Navbar className='invoice__noMatch custab'>
       <span>
@@ -80,7 +93,6 @@ const Invoices = () => {
       </span>
     </Navbar>
   );
-
   return (
     <>
       <FilterBar allInvoices={allInvoices} />
@@ -89,34 +101,23 @@ const Invoices = () => {
       ) : (
         <div className='row'>
           <div className='col-lg-12'>
-            <Table responsive='sm custab'>
-              <thead className='invoices__thead'>
-                <tr>
-                  <th className='text-center'> Invoice number</th>
-                  <th className='text-center'> Invoice date</th>
-                  <th className='text-center'> Recipient</th>
-                  <th className='text-center'> Vat amount </th>
-                  <th className='text-center'> Total amount</th>
-                  <th className='text-center'> Action</th>
-                </tr>
-              </thead>
-
-              {generaterInvoices()}
-            </Table>
+            {invoices.length !== 0 && generaterInvoices()}
             {isInvoicesListEmpty}
             <div className='pagination'>
-              <Pagination
-                activeLinkClass='active__page'
-                activePage={currentPage}
-                itemsCountPerPage={invoices.length}
-                totalItemsCount={totalItems}
-                pageRangeDisplayed={totalPages}
-                onChange={handlePageClick}
-                itemClass='page-item'
-                linkClass='page-link'
-                firstPageText='First'
-                lastPageText='Last'
-              />
+              {invoices.length !== 0 && (
+                <Pagination
+                  activeLinkClass='active__page'
+                  activePage={currentPage}
+                  itemsCountPerPage={invoices.length}
+                  totalItemsCount={totalItems}
+                  pageRangeDisplayed={totalPages}
+                  onChange={handlePageClick}
+                  itemClass='page-item'
+                  linkClass='page-link'
+                  firstPageText='First'
+                  lastPageText='Last'
+                />
+              )}
             </div>
           </div>
         </div>
