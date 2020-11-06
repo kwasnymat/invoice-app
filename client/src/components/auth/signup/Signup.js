6cf87d2a071684b3c9de105cd1a5db671b8811b7
unknown <krolczyk.artur@gmail.com>
2020-11-06 17:46:06 +0100

    added func to signup user

--- client/src/components/auth/signup/Signup.js
+++ client/src/components/auth/signup/Signup.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
+import { Form, Button } from 'react-bootstrap';
+import { ErrorMessage } from '@hookform/error-message';
+import { registerUser } from '../store/actions';
+import Loader from '../../layout/loader/Loader';
 
 import './Signup.scss';
 
 import { NavLink } from 'react-router-dom';
 
 const Signup = () => {
-  return (
-    <div className='signup-form'>
-      <form>
+  const { isLoading } = useSelector(({ shared }) => shared);
+
+  const { register, handleSubmit, errors, getValues } = useForm();
+
+  const dispatch = useDispatch();
+
+  const addUser = (userData) => {
+    dispatch(registerUser(userData));
+  };
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <Form className='signup-form' onSubmit={handleSubmit(addUser)}>
+      <div className='form__sign'>
         <h2>Sign Up</h2>
         <hr />
         <div className='form-group'>
@@ -17,13 +36,13 @@ const Signup = () => {
                 <span className='fa fa-user' />
               </span>
             </div>
-            <input
+            <Form.Control
               type='text'
-              className='form-control'
               name='username'
               placeholder='Username'
-              required='required'
+              ref={register({ required: 'Username is required.' })}
             />
+            <ErrorMessage errors={errors} name='username' as='p' />
           </div>
         </div>
         <div className='form-group'>
@@ -33,13 +52,13 @@ const Signup = () => {
                 <i className='fa fa-paper-plane'></i>
               </span>
             </div>
-            <input
+            <Form.Control
               type='email'
-              className='form-control'
               name='email'
               placeholder='Email Address'
-              required='required'
+              ref={register({ required: 'Email adress is required.' })}
             />
+            <ErrorMessage errors={errors} name='email' as='p' />
           </div>
         </div>
         <div className='form-group'>
@@ -49,28 +68,54 @@ const Signup = () => {
                 <i className='fa fa-lock'></i>
               </span>
             </div>
-            <input
-              type='text'
-              className='form-control'
+            <Form.Control
+              //   type='password'
               name='password'
               placeholder='Password'
-              required='required'
+              ref={register({ required: 'Password is required.' })}
             />
+            <ErrorMessage errors={errors} name='password' as='p' />
           </div>
         </div>
+
         <div className='form-group'>
-          <button type='submit' className='btn btn-primary btn-lg'>
-            Sign Up
-          </button>
+          <div className='input-group'>
+            <div className='input-group-prepend'>
+              <span className='input-group-text'>
+                <i className='fa fa-lock'></i>
+              </span>
+            </div>
+            <Form.Control
+              //   type='password'
+              name='passwordConfirmation'
+              placeholder='Confirm Password'
+              ref={register({
+                required: 'Please confirm password!',
+                validate: {
+                  matchesPreviousPassword: (value) => {
+                    const { password } = getValues();
+                    return password === value || 'Passwords should match!';
+                  },
+                },
+              })}
+            />
+            <ErrorMessage errors={errors} name='passwordConfirmation' as='p' />
+          </div>
         </div>
-      </form>
+
+        <div className='form-group'>
+          <Button type='submit' className='btn btn-primary btn-lg'>
+            Sign Up
+          </Button>
+        </div>
+      </div>
       <div className='text-center'>
         Already have an account?
         <NavLink exact to='/login'>
           Login here
         </NavLink>
       </div>
-    </div>
+    </Form>
   );
 };
 
