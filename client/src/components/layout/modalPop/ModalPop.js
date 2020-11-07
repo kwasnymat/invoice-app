8b368bf57e269ae276edc4374bd1a95329eacffb
unknown <krolczyk.artur@gmail.com>
2020-11-07 14:47:36 +0100

    added delete popup component

---
+++ client/src/components/layout/modalPop/ModalPop.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import './ModalPop.scss';
+
+const ModalPop = ({ deleteInvoiceHandler, closeModal, show, idInvoice }) => {
+  return (
+    <Modal show={show} onHide={closeModal} backdrop='static'>
+      <Modal.Body className='modal__message'>
+        <i className='far fa-question-circle' /> Are you sure want to delete
+        this Invoice?
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant='outline-warning'
+          onClick={() => {
+            deleteInvoiceHandler(idInvoice);
+            closeModal();
+          }}
+        >
+          Delete
+        </Button>
+        <Button variant='outline-warning' onClick={closeModal}>
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ModalPop;
