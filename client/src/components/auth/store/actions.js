e06b5678a3f98639650b0fef83513183f8cb6436
unknown <krolczyk.artur@gmail.com>
2020-11-07 18:25:27 +0100

    added redux for auth

--- client/src/components/auth/store/actions.js
+++ client/src/components/auth/store/actions.js
@@ -1,26 +1,83 @@
 import axios from 'axios';
+import { get } from 'react-hook-form';
 
 import * as types from './types';
 
-import { loaderOff, loaderOn, toasterOn } from '../../layout/store/actions';
+// import { loaderOff, loaderOn, toasterOn } from '../../layout/store/actions';
 
-export const registerUserSuccess = (payload) => {
+// export const registerUserSuccess = (payload) => {
+//   return {
+//     type: types.REGISTER_USER,
+//     payload,
+//   };
+// };
+
+// export const registerUserFail = (payload) => {
+//   return {
+//     type: types.REG_FAILED,
+//     payload,
+//   };
+// };
+
+export const getErrors = (errorMessage, errorStatus, idMessage = null) => {
   return {
-    type: types.REGISTER_USER,
-    payload,
+    type: types.GET_ERRORS,
+    payload: { errorMessage, errorStatus, idMessage },
   };
 };
 
-export const registerUserFail = (payload) => {
+export const clearErrors = () => {
   return {
-    type: types.REG_FAILED,
+    type: types.CLEAR_ERRORS,
+  };
+};
+
+export const userLoaded = (payload) => {
+  return {
+    type: types.USER_LOADED,
     payload,
   };
+};
+export const userLoading = () => {
+  return {
+    type: types.USER_LOADING,
+  };
+};
+
+export const loadUser = () => async (dispatch, getState) => {
+  dispatch(userLoading());
+  const token = getState().auth.token;
+  const configuration = {
+    headers: {
+      'Content=type': 'application/json',
+    },
+  };
+  if (token) {
+    configuration.headers['x-auth-token'] = token;
+  }
+  try {
+    const response = await axios.get(
+      'http://localhost:8080/auth/user',
+      configuration
+    );
+    dispatch(userLoaded(response.data));
+  } catch (err) {
+    dispatch(
+      getErrors(
+        err.response
+        // err.response.data,
+        // err.response.status
+      )
+    );
+    dispatch({
+      type: types.AUTH_ERROR,
+    });
+  }
 };
 
 export const registerUser = (userData) => async (dispatch) => {
   try {
-    dispatch(loaderOn());
+    // dispatch(loaderOn());
     const response = await axios.put(
       'http://localhost:8080/auth/signup',
       userData
@@ -28,12 +85,12 @@ export const registerUser = (userData) => async (dispatch) => {
     const { message } = response.data;
     const status = response.status;
     console.log(response);
-    dispatch(loaderOff());
-    dispatch(registerUserSuccess(response.data));
-    dispatch(toasterOn(message, status));
+    // dispatch(loaderOff());
+    // dispatch(registerUserSuccess(response.data));
+    // dispatch(toasterOn(message, status));
   } catch (err) {
-    dispatch(registerUserFail(err.response.data.message));
-    dispatch(toasterOn(err.response.data.message));
-    dispatch(loaderOff());
+    // dispatch(registerUserFail(err.response.data.message));
+    // dispatch(toasterOn(err.response.data.message));
+    // dispatch(loaderOff());
   }
 };
--- client/src/components/auth/store/reducer.js
+++ client/src/components/auth/store/reducer.js
@@ -3,32 +3,59 @@ import * as types from './types';
 const initialState = {
   token: localStorage.getItem('token'),
   isAuth: null,
-  loading: true,
-  error: null,
+  isLoading: false,
   user: null,
+  errorMessage: {},
+  errorStatus: null,
+  idMessage: null,
 };
 
 const invoiceReducer = (state = initialState, action) => {
   switch (action.type) {
-    case types.REGISTER_USER:
-      localStorage.setItem('token', action.payload.token);
+    case types.USER_LOADING:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case types.USER_LOADED:
+      return {
+        ...state,
+        isAuth: true,
+        isLoading: false,
+        user: action.payload,
+      };
+    case types.LOGIN_SUCCESS:
+    case types.REGISTER_SUCCESS:
       return {
         ...state,
         ...action.payload,
         isAuth: true,
-        loading: false,
+        isLoading: false,
       };
-    case types.REG_FAILED:
+    case types.AUTH_ERROR:
+    case types.LOGIN_FAIL:
+    case types.LOGOUT_SUCCESS:
+    case types.REGISTER_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
         token: null,
-        isAuth: false,
-        loading: false,
         user: null,
-        error: action.payload,
+        isAuth: false,
+        isLoading: false,
       };
-
+    case types.GET_ERRORS:
+      return {
+        errorMessage: action.payload.msg,
+        errorStatus: action.payload.status,
+        idMessage: action.payload.id,
+      };
+    case types.CLEAR_ERRORS:
+      return {
+        errorMessage: {},
+        errorStatus: null,
+        idMessage: null,
+      };
     default:
       return state;
   }
