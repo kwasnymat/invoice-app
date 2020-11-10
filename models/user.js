346480364f7c5790b0e807e2c2f018e3482b8588
unknown <krolczyk.artur@gmail.com>
2020-11-10 23:18:34 +0100

    changed user model

--- models/user.js
+++ models/user.js
@@ -14,10 +14,6 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
-  status: {
-    type: String,
-    default: 'New User',
-  },
   invoices: [
     {
       type: Schema.Types.ObjectId,
